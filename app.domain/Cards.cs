5e2f32b8de5a83457af1d7b16705a818d7a20493
Raquel <raquelsorila@gmail.com>
2021-01-14 21:09:45 +0800

    Cards and Accounts Model

---
+++ app.domain/Cards.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+namespace app.domain
+{
+    class Cards : BaseEntity
+    {
+        [DisplayName("Account ID")]
+        public int AccountID { get; set; }
+
+        public Accounts Accounts { get; set; }
+
+        [DisplayName("Card Classifications")]
+        [StringLength(50)]
+        public string CardClassifications { get; set; }
+
+        [DisplayName("Credit Points")]
+        public float CreditPoints { get; set; }
+
+        [DisplayName("Usage Number")]
+        public int UsageNumber { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        [Display(Name = "Expiration Date")]
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
+
+    }
+}
